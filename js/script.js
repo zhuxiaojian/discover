06b5ebcf1a0748858782ce268ce396e846489910
zhuxiaojian <zhuhaiyuan7@gmail.com>
2014-03-20 23:01:23 +0800

    一些说明

---
+++ js/script.js
@@ -0,0 +1,2 @@
+// 每个人都有成长的权利,也许他们此时很平凡,但请不要忽视他们,总有一天他们会站在你的面前对你说声谢谢.
+console && console.log('%c发现前端', 'font-size:36px;color:#fff;text-shadow:0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);');
